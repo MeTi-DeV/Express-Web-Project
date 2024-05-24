15280e3e8fd15a04d24c34f4a0a414e3d8b1c6f5
MeTi-DeV <mahdi_zarei78@yahoo.com>
2024-05-24 18:54:33 +0330

    work with ejs and define views for routes add boot-strap and ejs syntaxes

--- app/index.js
+++ app/index.js
@@ -1,18 +1,32 @@
-const http = require("http");
+const path = require("path");
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
 module.exports = class Application {
   constructor() {
     this.configServer();
+    this.setConfig();
+    this.setRoutes();
   }
   configServer() {
-    app.get("/", (req, res) => {
-      res.end("<h1>This is a Test</h1>");
-    });
-    const server = http.createServer(app);
     app.listen(3000, (err) => {
       if (err) console.log(err);
       console.log("server run on port 3000 ...");
+    });
+  }
+  setConfig() {
+    app.use(express.static(path.join(__dirname, "public")));
+    console.log(path.join(__dirname, "resource/views"));
+    app.set("view engine", "ejs");
+    app.set("views", path.join(__dirname, "resource/views"));
+    app.use(expressLayouts);
+    app.set("layout", "master");
+    app.set("layout extractScripts", true);
+    app.set("layout extractStyles", true);
+  }
+  setRoutes() {
+    app.get("/", (req, res) => {
+      res.render("index");
     });
   }
 };
